b24baa2e02fc45181cef63bdcb968ebf3d82a569
Aandrea <aandreab8@gmail.com>
2022-11-30 11:49:39 -0800

    some minor changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import "./App.css"
+import Home from "./routes/home/Home.jsx"
+import About from "./routes/about/About.jsx"
+import Portfolio from "./routes/portfolio/Portfolio.jsx"
+import Contact from "./routes/contact/Contact.jsx"
+import Footer from './components/footer';
+import { Link, Outlet, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+
+      <nav className="main-nav-container">
+        <div className='nav-links'>
+          <Link className="nav-link" to="/Home">Home</Link>
+          <Link className="nav-link" to="/Portfolio">Portfolio</Link>
+          <Link className="nav-link" to="/About">About</Link>
+          <Link className="nav-link" to="/Contact">Contact</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="Home" element={<Home />} />
+        <Route path="Portfolio" element={<Portfolio />} />
+        <Route path="About" element={<About />} />
+        <Route path="Contact" element={<Contact />} />
+      </Routes>
+      <Outlet />
+      <Footer/>
+    </main>
   );
 }
 
---
+++ src/components/footer.jsx
@@ -0,0 +1,8 @@
+import './footer.css'
+export default function Footer() {
+    return (
+        <footer className='footer'>
+            <div className="footer-container">hello footer</div>
+        </footer>
+    )
+}
